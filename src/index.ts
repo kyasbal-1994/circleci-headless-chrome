283fb555328d4ca431214a9f47a556c6298b5c2b
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-10-29 22:38:35 +0900

    fix: viewport test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
-import { launch } from "puppeteer";
-
+import { launch, Page } from "puppeteer";
+import { join } from "path";
+const exportDir = process.env.CIRCLE_ARTIFACTS || "ss";
 async function test() {
     const browser = await launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto("https://grimoire.gl");
-    await page.waitFor("canvas.gr-resource-loaded-canvas");
-    await page.screenshot({ path: "ss.png" });
+    await captureWithPage(page, "https://grimoire.gl");
     await browser.close();
+}
+
+async function captureWithPage(page: Page, addr: string) {
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(addr);
+    await page.waitFor("canvas.gr-resource-loaded-canvas");
+    await page.screenshot({ path: join(exportDir, Math.random() + ".png"), type: "png" });
 }
 
 test();
