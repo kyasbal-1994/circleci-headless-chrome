925d3313e2cbb1d4184a3fee8130b218e3e4bf46
Kakeru Ishii <i.kakeru@grimoire.gl>
2017-10-31 23:07:29 +0900

    fix: unhandled exception

--- diff.sh
+++ diff.sh
@@ -1,2 +1,2 @@
-mkdir "$CIRCLE_ARTIFACTS/diff"
-blink-diff --output "$CIRCLE_ARTIFACTS/diff/$1"  "$CIRCLE_ARTIFACTS/previous/$1"  "$CIRCLE_ARTIFACTS/$1"
+mkdir -p "$CIRCLE_ARTIFACTS/diff"
+blink-diff　--compose-ltr --output "$CIRCLE_ARTIFACTS/diff/$1"  "$CIRCLE_ARTIFACTS/previous/$1"  "$CIRCLE_ARTIFACTS/$1"
