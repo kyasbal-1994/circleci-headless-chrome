3853488450ab8e5b6ba20380d45f9a0065520bf5
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-13 11:04:33 +0900

    add uploading task

--- summarize.sh
+++ summarize.sh
@@ -1,5 +1,8 @@
-files=`find $CIRCLE_ARTIFACTS/current/ -name "*.png"`
+mkdir $CIRCLE_ARTIFACTS/all/
+aws s3 cp s3://$S3_BUCKET_URL/ss/$1 $CIRCLE_ARTIFACTS/all  --recursive --acl public-read
+files=`find $CIRCLE_ARTIFACTS/all/ -name "*.png"`
 count=`echo "$files" | wc -l`
 COL=5
 ROW=$(($count / $COL + 1))
 montage -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
+aws s3 cp $CIRCLE_ARTIFACTS/summary.jpg s3://$S3_BUCKET_URL/ss/$1
