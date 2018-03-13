eeedc4f0057dd7ad39e66b425269e6d7b127596d
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-13 10:58:43 +0900

    fix path problem

--- summarize.sh
+++ summarize.sh
@@ -1,5 +1,5 @@
-files=`find ./$CIRCLE_ARTIFACTS/current/ -name "*.png"`
+files=`find $CIRCLE_ARTIFACTS/current/ -name "*.png"`
 count=`echo "$files" | wc -l`
 COL=5
 ROW=$(($count / $COL + 1))
-montage -tile ${COL}x${ROW} $files ./$CIRCLE_ARTIFACTS/summary.jpg
+montage -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
