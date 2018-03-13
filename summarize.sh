fc7126781fce07a5175955ae96e5ea3f2c657fd4
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-13 12:32:16 +0900

    add label

--- summarize.sh
+++ summarize.sh
@@ -6,5 +6,5 @@ files=`find $CIRCLE_ARTIFACTS/all/ -name "*.png"`
 count=`echo "$files" | wc -l`
 COL=5
 ROW=$(($count / $COL + 1))
-montage -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
+montage -label '%f' -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
 aws s3 cp $CIRCLE_ARTIFACTS/summary.jpg s3://$S3_BUCKET_URL/ss/$SHA
