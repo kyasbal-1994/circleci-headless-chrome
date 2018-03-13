22dbdd2c565ee2baa1a3ae10046cb69cdefddd99
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-13 11:12:11 +0900

    add trigger reading

--- summarize.sh
+++ summarize.sh
@@ -1,8 +1,10 @@
 mkdir $CIRCLE_ARTIFACTS/all/
-aws s3 cp s3://$S3_BUCKET_URL/ss/$1 $CIRCLE_ARTIFACTS/all  --recursive --acl public-read
+SHA=`cat trigger.json| jq -r .sha`
+echo $SHA
+aws s3 cp s3://$S3_BUCKET_URL/ss/$SHA $CIRCLE_ARTIFACTS/all  --recursive --acl public-read
 files=`find $CIRCLE_ARTIFACTS/all/ -name "*.png"`
 count=`echo "$files" | wc -l`
 COL=5
 ROW=$(($count / $COL + 1))
 montage -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
-aws s3 cp $CIRCLE_ARTIFACTS/summary.jpg s3://$S3_BUCKET_URL/ss/$1
+aws s3 cp $CIRCLE_ARTIFACTS/summary.jpg s3://$S3_BUCKET_URL/ss/$SHA
