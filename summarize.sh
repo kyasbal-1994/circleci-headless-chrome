2c84ded14205e335bc65d7f232666fcc2fa2fe7f
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-13 12:47:35 +0900

    reformat summary.jpg

--- summarize.sh
+++ summarize.sh
@@ -4,7 +4,7 @@ echo $SHA
 aws s3 cp s3://$S3_BUCKET_URL/ss/$SHA $CIRCLE_ARTIFACTS/all  --recursive --acl public-read
 files=`find $CIRCLE_ARTIFACTS/all/ -name "*.png"`
 count=`echo "$files" | wc -l`
-COL=5
+COL=4
 ROW=$(($count / $COL + 1))
-montage -label '%f' -tile ${COL}x${ROW} $files $CIRCLE_ARTIFACTS/summary.jpg
+montage -label '%f' -tile ${COL}x${ROW} -geometry 512x512+20+20 $files $CIRCLE_ARTIFACTS/summary.jpg
 aws s3 cp $CIRCLE_ARTIFACTS/summary.jpg s3://$S3_BUCKET_URL/ss/$SHA
